8156408bbd66f9b24f4a38292052db87123bd0ac
YouFive <84190472+TheYouFive@users.noreply.github.com>
2022-08-22 01:21:42 +0900

    ssl

--- js/server.js
+++ js/server.js
@@ -1,63 +1,17 @@
-const app = require("express")();
 const https = require("https");
+const express = require("express");
 const fs = require("fs");
-const PORT = process.env.PORT || 443; // HTTPS 는 443 포트를 사용합니다
+
+const app = express();
+
 const options = {
-  key: fs.readFileSync(
-    __dirname + ".keys/publisher-v.site_20220821DD6C4.key.pem"
-  ),
-  cert: fs.readFileSync(
-    __dirname + ".keys/publisher-v.site_20220821DD6C4.crt.pem"
-  ),
-  ca: fs.readFileSync(__dirname + ".keys/ca-chain-bundle.pem"),
+  key: fs.readFileSync("./keys/publisher-v.site_20220821DD6C4.key.pem"),
+  cert: fs.readFileSync("./keys/publisher-v.site_20220821DD6C4.crt.pem"),
+  ca: fs.readFileSync("./keys/ca-chain-bundle.pem"),
 };
-// https 서버를 만들고 실행시킵니다
-https.createServer(options, app).listen(PORT);
 
-/* SSL option */
-// production 모드에서는 option 이 truthy한 값이고
-// development 모드에서는 option 이 falsy한 값입니다
-const option =
-  process.env.NODE_ENV === "production"
-    ? {
-        key: fs.readFileSync(
-          __dirname + ".keys/publisher-v.site_20220821DD6C4.key.pem"
-        ),
-        cert: fs.readFileSync(
-          __dirname + ".keys/publisher-v.site_20220821DD6C4.crt.pem"
-        ),
-        ca: fs.readFileSync(__dirname + ".keys/express/ca-chain-bundle.pem"),
-      }
-    : undefined;
+const server = https.createServer(options, app);
 
-// production 모드에서는 https 서버를
-// development 모드에서는 http 서버를 사용합니다
-option
-  ? https.createServer(option, app).listen(PORT, () => {
-      console.log(`Server is running at port ${PORT}`);
-    })
-  : http.createServer(app).listen(PORT, () => {
-      console.log(`Server is running at port ${PORT}`);
-    });
-
-// HTTPS 서버
-option
-  ? https.createServer(option, app).listen(PORT, () => {
-      console.log(`Server is running at port ${PORT}`);
-    })
-  : undefined;
-
-// HTTPS 서버로 요청을 전달하여 자동으로 SSL 연결을 해주는 HTTP 서버
-// SSL option 이 존재하지 않는 development 단계에서는 그냥 HTTP 서버만이 존재하게 됩니다.
-option
-  ? http
-      .createServer(function (req, res) {
-        res.writeHead(301, {
-          Location: "https://" + req.headers["host"] + req.url,
-        });
-        res.end();
-      })
-      .listen(80)
-  : http.createServer(app).listen(PORT, () => {
-      console.log(`Server is running at port ${PORT}`);
-    });
+server.listen(port, () => {
+  console.log("HTTPS server listening on port " + port);
+});

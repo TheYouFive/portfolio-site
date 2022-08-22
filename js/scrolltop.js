739015dd764f02d2bbc265ee72136ffd0b1fa7ed
YouFive <84190472+TheYouFive@users.noreply.github.com>
2022-08-23 04:43:51 +0900

    sidenav-update

---
+++ js/scrolltop.js
@@ -0,0 +1,32 @@
+$(window).on("scroll", function () {
+  var pos = $(window).scrollTop();
+  var pos2 = pos + 150;
+  var scrollBottom = pos + $(window).height();
+
+  // Link Highlighting
+  if (pos2 > $("#home").offset().top) {
+    highlightLink("");
+  }
+  if (pos2 > $("#aboutme").offset().top) {
+    highlightLink("aboutme");
+  }
+  if (pos2 > $("#skills").offset().top) {
+    highlightLink("skills");
+  }
+  if (pos2 > $("#portfolio").offset().top) {
+    highlightLink("portfolio");
+  }
+  if (
+    pos2 > $("#contact").offset().top ||
+    scrollBottom === $(document).height()
+  ) {
+    highlightLink("contact");
+  }
+});
+
+function highlightLink(anchor) {
+  $("side .active").removeClass("active");
+  $("side")
+    .find('[href="#' + anchor + '"]')
+    .addClass("active");
+}
